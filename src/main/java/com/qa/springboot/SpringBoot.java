f3ef6b95753b2f7f62de772b680296d597000ad6
武朋 <@DESKTOP-NQ7J6TU>
2019-10-28 16:10:59 +0800

    更改

---
+++ src/main/java/com/qa/springboot/SpringBoot.java
@@ -0,0 +1,12 @@
+package com.qa.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot.class, args);
+	}
+}
